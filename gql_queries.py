3c56fa4be63fd1314b47d74f23730cda7bf48d7e
Razvan Muresan <c_razvan.muresan@viasat.com>
2023-02-13 12:34:56 +0200

    add validations

--- gql_queries.py
+++ gql_queries.py
@@ -1,26 +1,31 @@
 from ariadne import convert_kwargs_to_snake_case
 
+from .validation.validators import Validator
+
 from .models.enums.work_order_type import WorkOrderType
 from .models.work_order import WorkOrder
 from .models.customer import Customer
 from .db import db
-import time
 
-def resolve_customers(obj, info):
+
+def resolve_customers(obj, info) -> object | None:
     return [customer.to_dict() for customer in Customer.query.all()]
 
-def resolve_work_orders(obj, info):
+
+def resolve_work_orders(obj, info) -> object | None:
     return [work_order.to_dict() for work_order in WorkOrder.query.all()]
 
+
 @convert_kwargs_to_snake_case
-def resolve_customer(obj, info, customer_id):
+def resolve_customer(obj, info, customer_id) -> object | None:
     customer = Customer.query.get(customer_id)
     if customer:
         return customer.to_dict()
     return None
 
+
 @convert_kwargs_to_snake_case
-def resolve_work_order(obj, info, work_order_id):
+def resolve_work_order(obj, info, work_order_id) -> object | None:
     work_order = WorkOrder.query.get(work_order_id)
     if work_order:
         return work_order.to_dict()
@@ -29,23 +34,38 @@ def resolve_work_order(obj, info, work_order_id):
 
 @convert_kwargs_to_snake_case
 def resolve_create_customer(obj, info, first_name, last_name, address, phone_number, email_address) -> object:
-    customer = Customer(
-        first_name=first_name,
-        last_name=last_name,
-        address=address,
-        phone_number=phone_number,
-        email_address=email_address
-    )
+    validator = Validator(db)
+    customer_fields = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'address': address,
+        'phone_number': phone_number,
+        'email_address': email_address
+    }
+    errors = validator.validate_customer(**customer_fields)
+    if errors:
+        return {"errors": errors}
+    customer = Customer(**customer_fields)
     db.session.add(customer)
     db.session.commit()
 
-    return customer.to_dict()
+    return {"customer": customer.to_dict()}
 
 
 @convert_kwargs_to_snake_case
 def resolve_create_work_order(obj, info, work_order_type, schedule, customer_id) -> object:
+    validator = Validator(db)
+    work_order_fields = {
+        "customer_id": customer_id,
+        "schedule": schedule,
+        "work_order_type": work_order_type
+    }
+    errors = validator.validate_work_order(**work_order_fields)
+    if errors:
+        return {"errors": errors}
     customer = Customer.query.get(customer_id)
-    work_order = WorkOrder(customer=customer.id, work_order_type=work_order_type, schedule=schedule)
+    work_order = WorkOrder(customer=customer.id,
+                           work_order_type=work_order_type, schedule=schedule)
     db.session.add(work_order)
     db.session.commit()
-    return work_order.to_dict()
+    return {"work_order": work_order.to_dict()}
---
+++ validation/validators.py
@@ -0,0 +1,59 @@
+import datetime
+import re
+
+from flask_sqlalchemy import SQLAlchemy
+from ..models.customer import Customer
+
+INVALID_EMAIL_ERROR = "Invalid email address format"
+INVALID_SCHEDULE_ERROR = "Invalid schedule time format"
+INVALID_PHONE_NUMBER_ERROR = "Invalid phone number format"
+INVALID_CUSTOMER_ID_ERROR = "Invalid customer ID"
+
+class Validator:
+    def __init__(self, db: SQLAlchemy):
+        self.db = db
+
+    def validate_customer(self, **args):
+        errors = [
+            self._validate_phone_number(args['phone_number']),
+            self._validate_email(args['email_address'])
+        ]
+        return self._filter_errors(errors)
+
+    def validate_work_order(self, **args):
+        errors = [
+            self._validate_customer_id(args['customer_id']),
+            self._validate_schedule(args['schedule'])
+        ]
+        return self._filter_errors(errors)
+
+    def _filter_errors(self, errors):
+        filtered_errors = filter(lambda error: error != None, errors)
+        if filtered_errors:
+            return [error for error in filtered_errors]
+        return None
+    
+    def _validate_schedule(self, schedule) -> str | None:
+        try:
+            datetime.datetime.strptime(schedule, "%d-%m-%Y")
+        except ValueError:
+            return INVALID_SCHEDULE_ERROR
+        return None
+
+    def _validate_email(self, email) -> str | None:
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if re.fullmatch(regex, email):
+            return None
+        return INVALID_EMAIL_ERROR
+
+    def _validate_phone_number(self, phone_number) -> str | None:
+        regex = r'^\+?(44)?(0|7)\d{9,13}$'
+        if re.fullmatch(regex, phone_number):
+            return None
+        return INVALID_PHONE_NUMBER_ERROR
+    
+    def _validate_customer_id(self, customer_id) -> str | None:
+        customer = self.db.session.get(Customer, customer_id)
+        if customer:
+            return None
+        return INVALID_CUSTOMER_ID_ERROR
